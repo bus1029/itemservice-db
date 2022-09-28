3af4d7d35a695b95aa3676ebd7f02ed9bd76b677
Seokhyun Bae <bus1029@gmail.com>
2022-09-28 17:38:38 +0900

    feat: H2 임베디드 모드 직접 사용
    - Test에 대해서만 H2 임베디드 모드를 직접 사용하도록 DataSource를
    빈으로 등록

--- src/main/kotlin/hello/itemservice/ItemServiceApplication.kt
+++ src/main/kotlin/hello/itemservice/ItemServiceApplication.kt
@@ -2,11 +2,15 @@ package hello.itemservice
 
 import hello.itemservice.config.JdbcTemplateV3Config
 import hello.itemservice.repository.ItemRepository
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Import
 import org.springframework.context.annotation.Profile
+import org.springframework.jdbc.datasource.DriverManagerDataSource
+import javax.sql.DataSource
 
 //@Import(MemoryConfig::class)
 //@Import(JdbcTemplateV1Config::class)
@@ -14,10 +18,23 @@ import org.springframework.context.annotation.Profile
 @Import(JdbcTemplateV3Config::class)
 @SpringBootApplication(scanBasePackages = ["hello.itemservice.web"])
 class ItemServiceApplication {
+  private val log: Logger = LoggerFactory.getLogger(this::class.java)
   @Bean
   @Profile("local")
   fun testDataInit(itemRepository: ItemRepository): TestDataInit {
     return TestDataInit(itemRepository)
+  }
+
+  @Bean
+  @Profile("test")
+  fun dataSource(): DataSource {
+    log.info("메모리 데이터베이스 초기화")
+    val dataSource = DriverManagerDataSource()
+    dataSource.setDriverClassName("org.h2.Driver")
+    dataSource.url = "jdbc:h2:mem:db;DB_CLOSE_DELAY=-1"
+    dataSource.username = "sa"
+    dataSource.password = ""
+    return dataSource
   }
 }
 
